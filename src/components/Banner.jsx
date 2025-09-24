9fa2aae9cd623bf81183cdfc3f92ec5108f49de9
AtikHasan16 <akhasannaeem@gmail.com>
2025-09-24 08:00:00 +0600

    added navigation bar, banner and button styles

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,11 +1,34 @@
-import React from 'react';
-
+import React from "react";
+import Container from "./Container";
+import assets from "../assets/pexels-daniela-elena-tentis-118658-691114.jpg";
 const Banner = () => {
-    return (
-        <div>
-           <h1>Banner</h1> 
+  return (
+    <Container>
+      <div
+        className="h-[]  bg-cover bg-center text-center text-white rounded-3xl"
+        style={{ backgroundImage: `url(${assets})` }}
+      >
+        <div className="bg-gradient-to-t from-black to-transparent p-30 rounded-3xl">
+          <h1 className="text-5xl font-bold pb-6">
+            Discover an exceptional cooking class tailored for you!
+          </h1>
+          <p className="text-lg">
+            Learn and Master Basic Programming, Data Structures, Algorithm, OOP,
+            Database and solve 500+ coding problems to become an exceptionally
+            well world-class Programmer.
+          </p>
+          <div className="flex justify-center items-center gap-5 py-10">
+            <button className="button-style text-black hover:text-white bg-green-300   hover:bg-transparent">
+              Explore Now{" "}
+            </button>
+            <button className="hover:text-black hover:bg-green-300 button-style">
+              Our Feedback
+            </button>
+          </div>
         </div>
-    );
+      </div>
+    </Container>
+  );
 };
 
 export default Banner;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,33 @@
 import React from "react";
+import Container from "./Container";
 
 const Navbar = () => {
   return (
-    <div>
-      <h1>Navigation Bar</h1>
-    </div>
+    <Container>
+      <div className=" flex gap-4 items-center justify-between my-10">
+        <div>
+          <h1 className="text-4xl font-bold">Recipe Calories</h1>
+        </div>
+        <div>
+          <ul className="flex md:gap-7 flex-col md:flex-row text-xl">
+            <li>Home</li>
+            <li>Recipe</li>
+            <li>About</li>
+            <li>Search</li>
+          </ul>
+        </div>
+        <div className="flex items-center justify-center gap-4">
+          <input
+            type="search"
+            placeholder="🔍 Search"
+            className="bg-gray-300 rounded-full p-3 hidden md:block"
+          />
+          <a href="#" className="rounded-full text-3xl bg-green-300 p-2">
+            🤴
+          </a>
+        </div>
+      </div>
+    </Container>
   );
 };
 
